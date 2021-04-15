2db0080ff45c23326eec1570ba731e194478fbc9
Knight, Eddie <iv.eddieknight@gmail.com>
2021-04-15 10:10:57 -0400

    Adjusted logic + added unit test for setStringVar

--- config/defaults.go
+++ config/defaults.go
@@ -58,28 +58,26 @@ func homeDir() string {
 // SetVar fetches the env var or sets the default value as needed for the specified field from VarOptions
 func SetVar(field interface{}, varName string, defaultValue interface{}) {
 	switch v := field.(type) {
+	case *string:
+		*field.(*string) = setStringVar(*field.(*string), varName, defaultValue.(string))
+	case *[]string:
+		*field.(*[]string) = setStringSliceVar(*field.(*[]string), varName, defaultValue.([]string))
 	default:
 		log.Fatalf("Unexpected value type provided for '%v', should be %T", varName, v)
-	case *string:
-		setStringVar(field.(*string), varName, defaultValue.(string))
-	case *[]string:
-		setStringSliceVar(field.(*[]string), varName, defaultValue.([]string))
 	}
 }
 
-func setStringVar(field *string, varName string, defaultValue string) {
-	value := *field
+func setStringVar(value string, varName string, defaultValue string) string {
 	if value == "" { // if field was empty, get value from env var
 		value = os.Getenv(varName)
 	}
 	if value == "" { // if still empty, use default value provided
 		value = defaultValue
 	}
-	field = &value
+	return value
 }
 
-func setStringSliceVar(field *[]string, varName string, defaultValue []string) {
-	value := *field
+func setStringSliceVar(value []string, varName string, defaultValue []string) []string {
 	if len(value) == 0 { // if field was empty, get value from env var
 		t := os.Getenv(varName) // for []string, env var should be comma separated values
 		if len(t) > 0 {
@@ -89,5 +87,5 @@ func setStringSliceVar(field *[]string, varName string, defaultValue []string) {
 	if len(value) == 0 { // if still empty, use default value provided
 		value = defaultValue
 	}
-	field = &value
+	return value
 }
