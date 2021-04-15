604f6a8f967f9e0d5f4701d179e71eddb1c88920
Knight, Eddie <iv.eddieknight@gmail.com>
2021-04-15 10:23:50 -0400

    Added test for setStringSliceVar

--- config/defaults_test.go
+++ config/defaults_test.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"os"
+	"reflect"
 	"testing"
 )
 
@@ -88,6 +89,48 @@ func Test_setStringVar(t *testing.T) {
 			value := setStringVar(vars.WriteDirectory, tt.varName, tt.defaultValue)
 
 			if value != tt.expectedReturnValue {
+				t.Errorf("setFromEnvOrDefaults(); Return Value = %v, Expected: %v", value, tt.expectedReturnValue)
+				return
+			}
+
+		})
+	}
+}
+
+func Test_setStringSliceVar(t *testing.T) {
+	defaultValue := []string{"one", "one", "two", "three", "five", "eight"}
+
+	tests := []struct {
+		testName            string
+		varName             string
+		envVarValue         string
+		expectedReturnValue []string
+	}{
+		{
+			testName:            "Test that set value is returned when provided",
+			varName:             "ENV_VAR_1",
+			envVarValue:         "one,two,three,four",
+			expectedReturnValue: []string{"one", "two", "three", "four"},
+		},
+		{
+			testName:            "Test that default value is returned when no value is provided",
+			varName:             "ENV_VAR_2",
+			envVarValue:         "",
+			expectedReturnValue: defaultValue,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			originalValue := os.Getenv(tt.varName) // Used to restore env to original state after test
+			defer func() {
+				os.Setenv(tt.varName, originalValue)
+			}()
+
+			os.Setenv(tt.varName, tt.envVarValue)
+
+			value := setStringSliceVar([]string{}, tt.varName, defaultValue)
+
+			if !reflect.DeepEqual(value, tt.expectedReturnValue) {
 				t.Errorf("setFromEnvOrDefaults(); Return Value = %v, Expected: %v", value, tt.expectedReturnValue)
 				return
 			}
