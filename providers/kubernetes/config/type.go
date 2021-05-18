761d511bf497c92ad708c2eb6ae6d0b8f9b95cae
Knight, Eddie <iv.eddieknight@gmail.com>
2021-05-18 17:36:51 -0400

    Added common kubernetes config options to provider

---
+++ providers/kubernetes/config/type.go
@@ -0,0 +1,34 @@
+package kubernetesconfig
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/probr/probr-sdk/config/setter"
+)
+
+type Kubernetes struct {
+	KeepPods                 string `yaml:"KeepPods"` // TODO: Change type to bool, this would allow us to remove logic from kubernetes.GetKeepPodsFromConfig()
+	KubeConfigPath           string `yaml:"KubeConfig"`
+	KubeContext              string `yaml:"KubeContext"`
+	AuthorisedContainerImage string `yaml:"AuthorisedContainerImage"`
+	ProbeNamespace           string `yaml:"ProbeNamespace"`
+}
+
+// setEnvOrDefaults will set value from os.Getenv and default to the specified value
+func (ctx *Kubernetes) SetEnvAndDefaults() {
+	// Notes on SetVar's values:
+	// 1. Pointer to local object; will be overwritten by env or default if empty
+	// 2. Name of env var to check
+	// 3. Default value to set if flags, vars file, and env have not provided a value
+
+	setter.SetVar(&ctx.KeepPods, "PROBR_KEEP_PODS", "false")
+	setter.SetVar(&ctx.KubeConfigPath, "KUBE_CONFIG", getDefaultKubeConfigPath())
+	setter.SetVar(&ctx.KubeContext, "KUBE_CONTEXT", "")
+	setter.SetVar(&ctx.AuthorisedContainerImage, "PROBR_AUTHORISED_IMAGE", "")
+}
+
+func getDefaultKubeConfigPath() string {
+	home, _ := os.UserHomeDir()
+	return filepath.Join(home, ".kube", "config")
+}
